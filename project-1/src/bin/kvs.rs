c71e13fa5eff8630e55e0cda2b9505e892341daf
Vedat Can Keklik <vcankeklik@gmail.com>
2023-08-20 12:28:34 +0300

    feat: Project-1 completed.

---
+++ project-1/src/bin/kvs.rs
@@ -0,0 +1,37 @@
+use clap::{Parser, Subcommand};
+use kvs::KvStore;
+use std::process::exit;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Get { key: String },
+    Set { key: String, value: String },
+    Rm { key: String },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Get { key } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Commands::Set { key, value } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Commands::Rm { key } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+    }
+}
